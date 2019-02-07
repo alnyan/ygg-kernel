5df8cb183ea13284e589650ccb973d76c6fab765
alnyan <alnyan@protonmail.com>
2019-02-07 18:13:30 +0200

    [all] move hw_init impl

--- src/arch/aarch64/board/bcm2837/board.c
+++ src/arch/aarch64/board/bcm2837/board.c
@@ -1,14 +1,12 @@
 #include "board.h"
 #include "arch/aarch64/mmio.h"
 
-int bcm2837_init_hw(void) {
+void hw_init(void) {
     // Setup UART0
     // Disable UART0
     bcm2837_uart0->cr = 0;
 
-    // Setup GPIO14/GPIO15
+    // Disable GPIO push-pull
     bcm2837_gpio->gppud = 0;
     delay(150);
-
-    return 0;
 }
--- src/kernel.c
+++ src/kernel.c
@@ -1,18 +1,6 @@
 #include "sys/debug.h"
+#include "arch/hw.h"
 #include "util.h"
-
-// Ugly right now, will move to "hw" header
-#if defined(ARCH_AARCH64) && defined(BOARD_BCM2837)
-#include "arch/aarch64/board/bcm2837/board.h"
-#endif
-
-int hw_init(void) {
-#ifdef ARCH_AARCH64
-    return bcm2837_init_hw();
-#else
-    return 0;
-#endif
-}
 
 void kernel_main(void) {
     hw_init();

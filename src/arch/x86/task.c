64963d3054126bd0ddaa812b3b2dbe27128a7c94
Mark <alnyan@airmail.cc>
2019-02-13 16:26:14 +0200

    [x86] add thread argument

--- src/arch/x86/task.c
+++ src/arch/x86/task.c
@@ -22,8 +22,8 @@ static uint32_t s_userStacks[256 * 4];
 uint32_t x86_tasks[4];
 int x86_task_index = 0;
 
-void task0(void) {
-    uint16_t *myptr = (uint16_t *) 0xC00B8000;
+void task0(void *arg) {
+    uint16_t *myptr = (uint16_t *) arg;
     *myptr = 0x0200 | 'A';
 
     while (1) {
@@ -34,67 +34,66 @@ void task0(void) {
     }
 }
 
-void task1(void) {
-    uint16_t *myptr = (uint16_t *) 0xC00B8002;
-    *myptr = 0x0700 | 'B';
+/*void task1(void) {*/
+    /*uint16_t *myptr = (uint16_t *) 0xC00B8002;*/
+    /**myptr = 0x0700 | 'B';*/
 
-    while (1) {
-        for (int i = 0; i < 0x0400000; ++i) {
-        }
+    /*while (1) {*/
+        /*for (int i = 0; i < 0x0400000; ++i) {*/
+        /*}*/
 
-        *myptr ^= 0x7700;
-    }
-}
-
-void task2(void) {
-    uint16_t *myptr = (uint16_t *) 0xC00B8004;
-    *myptr = 0x0400 | 'C';
-
-    while (1) {
-        for (int i = 0; i < 0x0400000; ++i) {
-        }
-
-        *myptr ^= 0x4400 | 'C';
-    }
-}
-
-void task3(void) {
-    uint16_t *myptr = (uint16_t *) 0xC00B8006;
-    *myptr = 0x0500 | 'D';
-
-    while (1) {
-        for (int i = 0; i < 0x0400000; ++i) {
-        }
-
-        *myptr ^= 0x5500;
-    }
-}
-/*void x86_task_switch(void) {*/
-    /*s_taskIndex = 1 - s_taskIndex;*/
+        /**myptr ^= 0x7700;*/
+    /*}*/
 /*}*/
 
-/*uint32_t x86_task_current(void) {*/
-    /*return s_tasks[s_taskIndex];*/
+/*void task2(void) {*/
+    /*uint16_t *myptr = (uint16_t *) 0xC00B8004;*/
+    /**myptr = 0x0400 | 'C';*/
+
+    /*while (1) {*/
+        /*for (int i = 0; i < 0x0400000; ++i) {*/
+        /*}*/
+
+        /**myptr ^= 0x4400 | 'C';*/
+    /*}*/
 /*}*/
 
-uint32_t x86_task_create(uint32_t addr) {
+/*void task3(void) {*/
+    /*uint16_t *myptr = (uint16_t *) 0xC00B8006;*/
+    /**myptr = 0x0500 | 'D';*/
+
+    /*while (1) {*/
+        /*for (int i = 0; i < 0x0400000; ++i) {*/
+        /*}*/
+
+        /**myptr ^= 0x5500;*/
+    /*}*/
+/*}*/
+
+uint32_t x86_task_create(void (*addr)(void *), void *arg) {
     uint32_t stackIndex = s_lastStack++;
     uint32_t ebp0 = (uint32_t) &s_stacks[stackIndex * X86_TASK_TOTAL_STACK + X86_TASK_TOTAL_STACK];
     uint32_t ebp3 = (uint32_t) &s_userStacks[stackIndex * 256 + 256];
     uint32_t cr3 = (uint32_t) &s_pagedirs[stackIndex * 1024];
     debug("Allocated task stack: 0x%x\n", ebp0);
     uint32_t *esp0 = (uint32_t *) ebp0;
+    uint32_t *esp3 = (uint32_t *) ebp3;
 
     memset((void *) cr3, 0, 1024 * 4);
     ((uint32_t *) cr3)[768] = 0x87;     // TODO: replace with actual kernel pagedir cloning
 
     cr3 -= KERNEL_VIRT_BASE;
 
+    // Push function argument
+    *--esp3 = (uint32_t) arg;
+    // Push some random return address, as return from userspace thread is NYI
+    *--esp3 = 0x12345678;
+
     *--esp0 = 0x23; // SS
-    *--esp0 = ebp3; // ESP
+    *--esp0 = (uint32_t) esp3; // ESP
     *--esp0 = 0x248;    // EFLAGS
     *--esp0 = 0x1B; // CS
-    *--esp0 = addr; // EIP
+    *--esp0 = (uint32_t) addr; // EIP
     // Task control params
 
     *--esp0 = 0;    // EAX
@@ -121,15 +120,13 @@ void x86_task_init(void) {
 
     s_lastStack = 0;
 
-    x86_tasks[0] = x86_task_create((uint32_t) task0);
-    x86_tasks[1] = x86_task_create((uint32_t) task1);
-    x86_tasks[2] = x86_task_create((uint32_t) task2);
-    x86_tasks[3] = x86_task_create((uint32_t) task3);
-
-    /*debug("task_create 0 = 0x%x\n", s_tasks[0]);*/
-    /*debug("task_create 1 = 0x%x\n", s_tasks[1]);*/
-
-    /*x86_tss_set(s_tasks[0] + 17 * 4);*/
+    for (int i = 0; i < 4; ++i) {
+        x86_tasks[i] = x86_task_create(task0, (void *) (0xC00B8000 + i * 2));
+    }
+    /*x86_tasks[0] = x86_task_create((uint32_t) task0);*/
+    /*x86_tasks[1] = x86_task_create((uint32_t) task1);*/
+    /*x86_tasks[2] = x86_task_create((uint32_t) task2);*/
+    /*x86_tasks[3] = x86_task_create((uint32_t) task3);*/
 }
 
 void task_start(void) {

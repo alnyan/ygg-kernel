c54afae7ac612d5efa26af344eb11999d1cb2a8f
Mark <alnyan@airmail.cc>
2019-02-13 22:59:58 +0200

    [all] remove mm testing code

--- src/kernel.c
+++ src/kernel.c
@@ -8,33 +8,6 @@ void kernel_main(void) {
     debug_init();
     hw_init();
 
-    // Alloc some pages
-    debug("Before alloc:\n");
-    mm_dump_pages(mm_current);
-
-    // Allocate contiguous region in kernel memory
-    uintptr_t v = mm_alloc_kernel_pages(mm_current, 2, MM_AFLG_RW | MM_AFLG_US);
-
-    if (v == MM_NADDR) {
-        debug("mm failed to alloc a page\n");
-    } else {
-        debug("mm allocd %p\n", v);
-    }
-
-    // Test some writing
-    for (int i = 0; i < (2 * MM_PAGESZ / 64); ++i) {
-        ((uint32_t *) v)[i * 16] = i;
-    }
-
-    debug("After alloc:\n");
-    mm_dump_pages(mm_current);
-
-    // Unmap contiguous region
-    mm_unmap_cont_region(mm_current, v, 2, MM_UFLG_PF);
-
-    debug("After unmap:\n");
-    mm_dump_pages(mm_current);
-
     irq_enable();
 
     while (1) {

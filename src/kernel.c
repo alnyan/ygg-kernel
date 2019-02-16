9c2d4e4adf34ac33be6ce96dcf1abb5d68443bd2
Mark <alnyan@airmail.cc>
2019-02-16 15:56:29 +0200

    [all] remove test code

--- src/kernel.c
+++ src/kernel.c
@@ -10,28 +10,6 @@ void kernel_main(void) {
     debug_init();
     hw_init();
 
-    struct heap_stat st;
-    heap_stat(&st);
-    debug("T = %u\n", st.total);
-    heap_dump();
-
-    void *p0 = heap_alloc(512);
-    void *p1 = heap_alloc(512);
-    void *p2 = heap_alloc(512);
-    heap_free(p1);
-    heap_dump();
-
-    p0 = heap_realloc(p0, 512 + 511);
-    debug("After realloc\n");
-    heap_dump();
-
-    heap_free(p0);
-    heap_free(p2);
-
-    heap_stat(&st);
-    debug("T = %u\n", st.total);
-    heap_dump();
-
     irq_enable();
 
     while (1) {

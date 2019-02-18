f176baef92737f31bd12727b53f0b292673be093
Mark <alnyan@airmail.cc>
2019-02-18 14:04:47 +0200

    [vfs] remove /dev/zero testing code

--- src/kernel.c
+++ src/kernel.c
@@ -16,24 +16,6 @@ void kernel_main(void) {
 
     // Now the kernel-stuff kicks in
     devfs_init();
-    if (vfs_mount(NULL, "/dev", vfs_devfs, 0) != 0) {
-        panic("Failed to mount devfs\n");
-    }
-
-    vfs_file_t *f;
-    assert(f = vfs_open("/dev/zero", VFS_FLG_RD));
-
-    char buf[16];
-    for (char c = 1; c < sizeof(buf); ++c) {
-        buf[c - 1] = c * 13;
-    }
-    debug_dump(buf, sizeof(buf));
-
-    assert(vfs_read(f, buf, sizeof(buf)) == 16);
-
-    debug_dump(buf, sizeof(buf));
-
-    vfs_close(f);
 
     // This is where we're ready to accept the first interrupt and start multitasking mode
     irq_enable();

2044b9543b37b015489fc0b6899b25a293405e4c
Mark <alnyan@airmail.cc>
2019-02-19 16:43:50 +0200

    [x86] remove fork() testing code

--- usr/bin/init.c
+++ usr/bin/init.c
@@ -7,51 +7,36 @@ void _start(void *arg) {
     char line[256];
     size_t pos = 0;
 
-    printf("1\n");
+    while (1) {
+        printf("> ");
 
-    int r = fork();
+        while (1) {
+            if (read(0, &keybuf, 1) != 1) {
+                exit(-1);
+            }
 
-    if (r == 0) {
-        r = fork();
-        if (r) {
-            printf("%d\n", r);
+            if (keybuf >= ' ') {
+                line[pos++] = keybuf;
+                write(0, &keybuf, 1);
+            } else if (keybuf == '\b') {
+                if (pos) {
+                    line[pos--] = 0;
+                    write(0, &keybuf, 1);
+                }
+            } else if (keybuf == '\n') {
+                write(0, &keybuf, 1);
+                line[pos] = 0;
+                pos = 0;
+                break;
+            }
         }
-    } else {
-        printf("%d\n", r);
+
+        printf("You've typed: \"%s\"\n", line);
+
+        if (!strcmp(line, "crashme") || !strcmp(line, "exit")) {
+            exit(1234);
+        }
     }
-
-    while (1);
-
-//    while (1) {
-//        printf("> ");
-//
-//        while (1) {
-//            if (read(0, &keybuf, 1) != 1) {
-//                exit(-1);
-//            }
-//
-//            if (keybuf >= ' ') {
-//                line[pos++] = keybuf;
-//                write(0, &keybuf, 1);
-//            } else if (keybuf == '\b') {
-//                if (pos) {
-//                    line[pos--] = 0;
-//                    write(0, &keybuf, 1);
-//                }
-//            } else if (keybuf == '\n') {
-//                write(0, &keybuf, 1);
-//                line[pos] = 0;
-//                pos = 0;
-//                break;
-//            }
-//        }
-//
-//        printf("You've typed: \"%s\"\n", line);
-//
-//        if (!strcmp(line, "crashme") || !strcmp(line, "exit")) {
-//            exit(1234);
-//        }
-//    }
 
     exit(-1);
 }

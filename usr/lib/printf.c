e678bd65d0d2a7016094cb6f6b8f41fd67b4da17
Mark <alnyan@airmail.cc>
2019-03-04 15:35:34 +0200

    [usr/libc] expand printf family

--- usr/lib/printf.c
+++ usr/lib/printf.c
@@ -1,13 +1,43 @@
 #include "stdio.h"
 #include "unistd.h"
 #include "string.h"
-void printf(const char *fmt, ...) {
+
+int printf(const char *fmt, ...) {
     char buf[1024];
 
     va_list args;
     va_start(args, fmt);
-    vsnprintf(buf, sizeof(buf), fmt, args);
+    int r = vsnprintf(buf, sizeof(buf), fmt, args);
     va_end(args);
 
-    write(STDOUT_FILENO, buf, strlen(buf));
+    return write(STDOUT_FILENO, buf, r);
+}
+
+int dprintf(int fd, const char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    int r = vsnprintf(buf, sizeof(buf), fmt, args);
+    va_end(args);
+    return write(fd, buf, r);
+}
+
+int snprintf(char *buf, size_t len, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    int r = vsnprintf(buf, len, fmt, args);
+    va_end(args);
+    return r;
+}
+
+int vprintf(const char *fmt, va_list args) {
+    char buf[1024];
+    int r = vsnprintf(buf, sizeof(buf), fmt, args);
+    return write(STDOUT_FILENO, buf, r);
+}
+
+int vdprintf(int fd, const char *fmt, va_list args) {
+    char buf[1024];
+    int r = vsnprintf(buf, sizeof(buf), fmt, args);
+    return write(fd, buf, r);
 }
--- usr/lib/stdio.h
+++ usr/lib/stdio.h
@@ -3,5 +3,10 @@
 #include <stddef.h>
 
 // Not like libc, but works for us
-void printf(const char *fmt, ...);
+int printf(const char *fmt, ...);
+int snprintf(char *buf, size_t len, const char *fmt, ...);
+int dprintf(int fd, const char *fmt, ...);
+
 int vsnprintf(char *dst, size_t len, const char *fmt, va_list args);
+int vprintf(const char *fmt, va_list args);
+int vdprintf(int fd, const char *fmt, va_list args);
